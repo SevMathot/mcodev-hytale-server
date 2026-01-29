6ae53a86e2666554ab86c26c284ee8e1c0f6e5ae
Sev Mato <28763268+SevMathot@users.noreply.github.com>
2026-01-29 08:32:14 +0100

    Update launch.sh
    Copyright + Error check

--- launch.sh
+++ launch.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# ====   Copyright 2026, mCoDev Systems, All rights reserved.   ====
+# =  Purpose:    Calls tokens and then launches the game server.
+# =  Reference:  
+# =  Author:     Steve (Sev) Mathot, mCoDev Systems. (www.mcodev.net
+# =  License:    mCoDev Systems General Public License (MIT)
+# =  Usage:      ./launch.sh
+# =  Output:     
+# ==================================================================
+
 source ./tokens.sh
+
+if [ $? -ne 0 ]; then
+    echo "Unable to fetch tokens."
+    exit 1
+fi
+
 ./start.sh "$@"
